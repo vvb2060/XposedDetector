7ab131731c220a42d6d96e957167b9e924ec78c1
vvb2060 <vvb2060@gmail.com>
2021-03-25 01:50:27 +0800

    Update AGP

--- app/src/main/java/io/github/vvb2060/xposeddetector/MainActivity.java
+++ app/src/main/java/io/github/vvb2060/xposeddetector/MainActivity.java
@@ -2,27 +2,34 @@ package io.github.vvb2060.xposeddetector;
 
 import android.app.Activity;
 import android.os.Bundle;
-
-import io.github.vvb2060.xposeddetector.databinding.ActivityMainBinding;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        var layout = new RelativeLayout(this);
+        var textView = new TextView(this);
         switch (getXposedStat()) {
             case 0:
-                binding.textView.setText(R.string.no_xposed);
+                textView.setText(R.string.no_xposed);
                 break;
             case 1:
-                binding.textView.setText(R.string.found_xposed);
+                textView.setText(R.string.found_xposed);
                 break;
             case 2:
-                binding.textView.setText(R.string.antied_xposed);
+                textView.setText(R.string.antied_xposed);
                 break;
         }
+        var params = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        params.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        params.addRule(RelativeLayout.CENTER_VERTICAL);
+        layout.addView(textView, params);
+        setContentView(layout);
     }
 
-    native int getXposedStat();
+    private native int getXposedStat();
 }
